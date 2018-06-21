a0a89c22f40fa26869734edfedc2274f11d1a04e
Mateusz Sawastian <m.sawastian@gmail.com>
2018-06-21 16:31:52 +0200

    Styling fixes

--- jsx/display-pollution.js
+++ jsx/display-pollution.js
@@ -4,12 +4,23 @@ const DisplayPollution = props => {
     return (
             <section className={'pollution-container'}>
                 <div className={'pollution-top-row'}>
-                    <div className={'pollution-logo'}>TEST</div>
-                    <div className={'pollution-index-overall'}>{Math.round(props.airlyData.airQualityIndex)}</div>
+                    <div className={'pollution-logo'}></div>
+                    <div className={'pollution-index-overall'}>
+                        <div className={'pollution-index-overall-value'}>{Math.round(props.airlyData.airQualityIndex)}</div>
+                        <div className={'pollution-index-pm-value-unit'}>INDEX</div>
+                    </div>
                 </div>
                 <div className={'pollution-bottom-row'}>
-                    <div className={'pollution-index-10'}>{Math.round(props.airlyData.pm10)} &#181;m/m3</div>
-                    <div className={'pollution-index-25'}>{Math.round(props.airlyData.pm25)} &#181;m/m3</div>
+                    <div className={'pollution-index-pm'}>
+                        <div className={'pollution-index-pm-value'}>{Math.round(props.airlyData.pm10)}
+                            <div className={'pollution-index-pm-value-unit'}>&#181;m/m3 PM10</div>
+                        </div>
+                    </div>
+                    <div className={'pollution-index-pm'}>
+                        <div className={'pollution-index-pm-value'}>{Math.round(props.airlyData.pm25)}
+                            <div className={'pollution-index-pm-value-unit'}>&#181;m/m3 PM2.5</div>
+                        </div>
+                    </div>
                 </div>
             </section>
     )
