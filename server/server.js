11fa8785a5a7fba16ad3752f5455090ff1f3e21c
m.sawastian <m.sawastian@gmail.com>
2018-07-30 18:59:59 +0200

    Further node development

--- server/server.js
+++ server/server.js
@@ -6,4 +6,19 @@ const port = process.env.PORT;
 
 
 app.use(express.static(__dirname + './../'));
+
+
+app.get('/api/weather/', (request, response) => {
+    const locationName = request.body.locationName,
+        latitude = request.body.latitude,
+        longitude = request.body.longitude;
+
+    response.send({
+        locationName,
+        latitude,
+        longitude
+    })
+});
+
+
 app.listen(port);
