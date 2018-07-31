6023840cc751215a8c408bacc565aa4d0e88be61
m.sawastian <m.sawastian@gmail.com>
2018-07-31 22:12:41 +0200

    Airly data fetch now server-side.

--- server/server.js
+++ server/server.js
@@ -15,7 +15,6 @@ app.get('/api/weather/coordinates', (request, response) => {
 
     let allData = {};
 
-
         axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=1564f8b4dd2a1779efdc16350e54fe25`)
             .then(data => {
                 allData = {
@@ -29,9 +28,29 @@ app.get('/api/weather/coordinates', (request, response) => {
                     forecastData: data.data
                 };
 
-                response.send(allData)
+                return axios.get(`https://airapi.airly.eu/v1/nearestSensor/measurements?latitude=${latitude}&longitude=${longitude}`, {
+                    headers: {
+                        Accept: 'application/json',
+                        apikey: 'Z6ObIaiUCKIaZAYUbOXUvzzTjAi8Xl3j'
+                    }
+                })
+
+
+            })
+            .then(data => {
+                allData = {
+                    ...allData,
+                    airlyData: data.data
+                };
+
+
+
+
             })
             .catch(error => response.send(error))
+
+    // response.send(allData)
+
 });
 
 
