67cd65fbf933e8b01830db016fc3d99ef12d52f0
m.sawastian <m.sawastian@gmail.com>
2018-08-03 16:19:06 +0200

    Production bundle.js

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
         filename: "./bundle.js",
         path: path.resolve(__dirname, "dist")
     },
-    mode: "development",
+    mode: "production",
     watch: true,
     devtool: 'source-map',
     module: {
